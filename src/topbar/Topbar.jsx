2cd7367b78956d276e5e729615596cc16812c69c
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-05 17:20:51 +0530

    font awesome added

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -1,6 +1,17 @@
 import React from "react";
-import "./topbar.css"
+import "./topbar.css";
 
 export default function Topbar() {
-  return <div className="top">Topbar</div>;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i class="fa-brands fa-square-facebook"></i>
+        <i class="fa-brands fa-square-twitter"></i>
+        <i class="fa-brands fa-square-pinterest"></i>
+        <i class="fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="topCenter">Center</div>
+      <div className="topRight">Right</div>
+    </div>
+  );
 }
