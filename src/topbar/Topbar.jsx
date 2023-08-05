8152cca1c135f36ca498272dbf9d290b3f4fde88
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-05 18:04:00 +0530

    Navbar

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -5,13 +5,29 @@ export default function Topbar() {
   return (
     <div className="top">
       <div className="topLeft">
-        <i class="fa-brands fa-square-facebook"></i>
-        <i class="fa-brands fa-square-twitter"></i>
-        <i class="fa-brands fa-square-pinterest"></i>
-        <i class="fa-brands fa-square-instagram"></i>
+        <i className="topIcon fa-brands fa-square-facebook"></i>
+        <i className="topIcon fa-brands fa-square-twitter"></i>
+        <i className="topIcon fa-brands fa-square-pinterest"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
       </div>
-      <div className="topCenter">Center</div>
-      <div className="topRight">Right</div>
+
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+
+      <div className="topRight">
+        <img
+          className="topImg"
+          src="https://pbs.twimg.com/profile_images/1283759937177317376/iKqA9lOB_400x400.jpg"
+        />
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   );
 }
