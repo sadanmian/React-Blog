47f037b7bf4fa17b9ccadb9e60f84f8c3f8d5ac5
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-05 23:38:47 +0530

    Write Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <Topbar />
       {/* <Home /> */}
-      <Single />
+      {/* <Single /> */}
+      <Write />
     </>
   );
 }
