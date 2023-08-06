f9905a58235202399184a65718ccb154ff9778e8
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-06 18:35:26 +0530

    Settings Components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Topbar />
       {/* <Home /> */}
       {/* <Single /> */}
-      <Write />
+      {/* <Write /> */}
+      <Settings />
     </>
   );
 }
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img src="https://pbs.twimg.com/profile_images/1283759937177317376/iKqA9lOB_400x400.jpg" />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon fa-regular fa-circle-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Sadan" />
+          <label>Email</label>
+          <input type="email" placeholder="Sadan@gmail.com" />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
